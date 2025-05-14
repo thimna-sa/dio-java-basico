8b9fcb7a4908a1854b1c24966425b97707a16f3f
thimna.sa <thimnasantos@gmail.com>
2025-05-14 18:45:49 -0300

    add boletim escolar

---
+++ edu/thimna/modulo.java/BoletimEscolar.java
@@ -0,0 +1,23 @@
+package edu.thimna.modulo.java;
+
+public class BoletimEscolar {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int media = 5;
+        if (media < 6) {
+            System.out.println("REPROVADO") ;
+        else if (media ==6 ) {
+            System.out.println("prova minerva") ;
+        else  {
+            System.out.println("Aprovado") ;
+        }
+            
+        }
+            
+        }
+    }
+    
+}
