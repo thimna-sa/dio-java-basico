f1d63eb5823f3050709ebb70a79f24c9a5aa9b64
thimna.sa <thimnasantos@gmail.com>
2025-05-13 21:05:52 -0300

    meu primeiro script

---
+++ MinhaClasse.java
@@ -0,0 +1,20 @@
+public class MinhaClasse {
+
+public static void main (String[] aStrings){
+        
+        String primeiroNome = "thimna";
+        String segundoNome = "Santos";
+
+        String nomenomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        SySystem.out.println(nomeCompleto);
+
+
+
+
+    }
+
+
+
+
+}
