eb1957b6d7ed44787a69dd9a7223efa5809c0757
thimna.sa <thimnasantos@gmail.com>
2025-05-13 21:19:30 -0300

    funcionando 2

--- MinhaClasse.java
+++ MinhaClasse.java
@@ -3,7 +3,7 @@ public class MinhaClasse {
 public static void main (String  [] args)   {
         
         String primeiroNome = "thimna";
-        String segundoNome = "Santos";
+        String segundoNome = " Santos";
 
         String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
 
@@ -11,9 +11,10 @@ public static void main (String  [] args)   {
     }
 
 public static String nomeCompleto (String primeiroNome, String segundoNome) {
-    return primeiroNome.concat(" ").concat(segundoNome);
+   return "Resultado do metodo" + pprimeiroNome.concat("").concat(segundoNome);
 
-}
+
+}   
 
 
 }
