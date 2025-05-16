2a34e47515174ed77b39f4d73238ea0c5ca4bbc9
thimna.sa <thimnasantos@gmail.com>
2025-05-16 20:27:05 -0300

    estruturas de controle

--- TesteOperadores.java
+++ TesteOperadores.java
@@ -1,25 +1,35 @@
 public class TesteOperadores {
 
-    public static void main(String[] args) {
-     String nomeCompleto = "linguagem" + " Java";
-     System.out.println(nomeCompleto);
+    //public static void main(String[] args) {
+     //String nomeCompleto = "linguagem" + " Java";
+    // System.out.println(nomeCompleto);
      
 
-     String concatenacao = "?";
-     concatenacao = 1+1+1+"1";
-      System.out.println(concatenacao);
+    // String concatenacao = "?";
+     //concatenacao = 1+1+1+"1";
+      //System.out.println(concatenacao);
 
-     concatenacao = 1+ "1"+ 1 + 1;
-      System.out.println(concatenacao);
+     //concatenacao = 1+ "1"+ 1 + 1;
+      //System.out.println(concatenacao);
 
-     concatenacao = "1"+ 1 + 1 + 1;
+     //concatenacao = "1"+ 1 + 1 + 1;
 
-      System.out.println(concatenacao);
+     /// System.out.println(concatenacao);
 
-     concatenacao = "1" + (1+1+1);
+     //concatenacao = "1" + (1+1+1);
 
-       System.out.println(concatenacao);
+      // System.out.println(concatenacao);
 
-    }
+   // }
     
+   public static void main( String [] args) {
+    int idade = 15;
+
+    if (idade >= 18 ) {
+      System.out.println("Já pode ser preso");
+    }
+    else{
+      System.out.println("de menor vai dormir");
+    }
+   }
 }
